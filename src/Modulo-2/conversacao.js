8cc814d9d832534b44894b9526b2e36fc9b6f892
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-03-17 22:33:48 -0300

    Demo bot usando connector & Dialog via Emulator.

---
+++ src/Modulo-2/conversacao.js
@@ -0,0 +1,39 @@
+/**
+ * Arquivo: conversacaoBot.js
+ * Data: 17/03/2018
+ * Descrição: Desenvolvimento de um Bot usando Dialog via Emulator
+ * Author: Glaucia Lemos
+ *
+ */
+
+const restify = require('restify');
+const builder = require('botbuilder');
+
+//Configuração do Server via restify:
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log('%s Aplicação executando na porta %s', server.name, server.url);
+});
+
+const connector = new builder.ChatConnector({
+    appId: '',
+    appPassword: ''
+});
+
+const bot = new builder.UniversalBot(connector, [
+    (session) => {
+        builder.Prompts.text(session,'Olá! Tudo bem?');
+    },
+
+    (session) => {
+    builder.Prompts.text(session, 'Como você se chama?');
+    },
+
+    (session, results) => {
+        let msg = results.response;
+        session.send(`Oi ${msg}! Em que posso ajudar?`);
+    }
+]);
+
+//Endpoint para executar as mensagens para o usuário:
+server.post('api/messages', connector.listen());
